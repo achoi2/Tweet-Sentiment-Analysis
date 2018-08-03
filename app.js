f269399010b0375b3ce91ff046367fbf0196151b
Andrew <andrewchoi1@hotmail.com>
2018-08-03 09:54:55 -0400

    changed spacing fixed variablenames

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
 var request = require("request");
 
-var watsoneUsername = "";
+var watsonUsername = "";
 var watsonPassword = '';
-var watsonUrl =
-    'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21';
+var watsonUrl = 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21';
 
 var data = {};
 data.text = "I hate you";
 
 request.post(
-    {
-        url: watsoneUrl,
-        json: data,
-        auth: {
-            user: watsonUsername,
-            pass: watsonPassword
-        }
-    },
+    { url: watsonUrl, json: data, auth: { user: watsonUsername, pass: watsonPassword} },
     function(err, response, body) {
         console.log(body);
-
-        console.log(
-            body.document_tone.tones[0].tone_name +
-                ', Score: ' +
-                body.document_tone.tones[0].score
-        );
+        console.log(body.document_tone.tones[0].tone_name + ', Score: ' + body.document_tone.tones[0].score);
     }
 );
