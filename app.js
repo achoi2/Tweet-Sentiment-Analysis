8f1052d53b552f0a66d179a7aca0e5c3e43f5cd2
Sara Muntean <munteansr@gmail.com>
2018-08-03 14:34:08 -0400

    Added function to pull tone data from Watson into object we can access easily.

--- app.js
+++ app.js
@@ -3,14 +3,34 @@ var watsonUrl = 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?ve
 var data = {};
 data.text = "I hate you.";
 
+var toneChartObject = {anger: 0,
+                        fear: 0,
+                        joy: 0,
+                        sadness: 0,
+                        analytical: 0,
+                        confident: 0,
+                        tentative: 0
+                        };
+
+var toneArray = ['anger', 'fear', 'joy', 'sadness', 'analytical', 'confident', 'tentative'];
+
 $.ajax(
     { url: watsonUrl, 
     data: data, 
     headers: { 
         "Authorization": "Basic " + btoa(watsonUsername + ":" + watsonPassword) },
-    function(err, response, body) {
-        console.log(body);
-        console.log(body.document_tone.tones[0].tone_name + ', Score: ' + body.document_tone.tones[0].score);
+    success: function(watsonData) {
+        var toneObject = watsonData.document_tone.tones 
+        for (var i = 0; i < toneObject.length; i++) {
+            watsonToneId = toneObject[i].tone_id
+            for (var j = 0; j < toneArray.length; j++) {
+                toneName = toneArray[j]
+                if (watsonToneId === toneName) {
+                    toneChartObject[toneName] = toneObject[i].score;
+                }
+            }  
+        }      
     }
     });
+
 
