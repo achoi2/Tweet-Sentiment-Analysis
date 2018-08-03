d3e43626fca2c085aab9c52bbb58e95883c693a6
Hannah <hrglasser94@gmail.com>
2018-08-03 14:45:59 -0400

    Created chart with temporary example data points. Began styling chart.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var watsonUrl = 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21';
+var ctx = document.getElementById('watsonChart').getContext('2d');
 
 var data = {};
 data.text = "I hate you.";
@@ -14,3 +15,51 @@ $.ajax(
     }
     });
 
+var mixedChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            label: 'Tone Score',
+            data: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6],
+            backgroundColor: 'rgb(65, 193, 244, 0.5)',
+            borderColor: 'rgb(65, 193, 244, 0.5)'
+            }, {
+            label: 'Minimum Approval Score',
+            data: [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2],
+            // Changes this dataset to become a line
+            type: 'line',
+            fill: false,
+            backgroundColor: 'rgb(244, 72, 66)',
+            borderColor: 'rgb(244, 72, 66)',
+            pointRadius: 5,
+            // showLine: false
+            }, {
+            label: 'Target Approval Score',
+            data: [0.4, 0.6, 0.5, 0.4, 0.3, 0.3, 0.6],
+            type: 'line',
+            fill: false, 
+            pointRadius: 5,
+            backgroundColor: 'rgb(54, 216, 36)',
+            borderColor: 'rgb(54, 216, 36)',
+            }, {
+            label: 'Maximum Approval Score',
+            data: [0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6],
+            type: 'line',
+            fill: false,
+            pointRadius: 5,
+            // showLine: false
+            backgroundColor: 'rgb(244, 72, 66)',
+            borderColor: 'rgb(244, 72, 66)'
+            }],
+        labels: ['Anger', 'Fear', 'Joy', 'Sadness', 'Analytical', 'Confident', 'Tentative']
+    },
+    options: {
+        beginatZero: true, 
+        yAxes: [{
+            max: 1.0
+        }]
+        // scales: {
+
+        // }
+    }
+});
