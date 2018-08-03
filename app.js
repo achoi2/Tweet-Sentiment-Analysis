f9c5105cf7c2e0182f5bc5ba2ac5014ad819e1ca
Andrew <andrewchoi1@hotmail.com>
2018-08-03 09:31:14 -0400

    changed variable names

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 var request = require("request");
 
-var toneUsername = "";
-var tonePassword = "";
-var toneUrl = "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21";
+var watsoneUsername = "";
+var watsonPassword = '';
+var watsonUrl =
+    'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21';
 
 var data = {};
 data.text = "I hate you";
 
-request.post({
-    url: toneUrl,
-    json: data,
-    auth: {
-        user: toneUsername,
-        pass: tonePassword
+request.post(
+    {
+        url: watsoneUrl,
+        json: data,
+        auth: {
+            user: watsonUsername,
+            pass: watsonPassword
+        }
+    },
+    function(err, response, body) {
+        console.log(body);
+
+        console.log(
+            body.document_tone.tones[0].tone_name +
+                ', Score: ' +
+                body.document_tone.tones[0].score
+        );
     }
-
-}, function (err, response, body) {
-    console.log(body);
-
-    console.log(body.document_tone.tones[0].tone_name +
-        ", Score: " + body.document_tone.tones[0].score);
-});
+);
