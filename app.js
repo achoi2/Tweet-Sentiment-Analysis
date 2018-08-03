81496b0cf771fd823f2f704add7724a550fc9d08
Sara Muntean <munteansr@gmail.com>
2018-08-03 15:09:34 -0400

    Capitalized toneNames and alphabetized object.

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ var watsonUrl = 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?ve
 var data = {};
 data.text = "I hate you.";
 
-var toneChartObject = {anger: 0,
-                        fear: 0,
-                        joy: 0,
-                        sadness: 0,
-                        analytical: 0,
-                        confident: 0,
-                        tentative: 0
+var toneChartObject = {Analytical: 0,
+                        Anger: 0,
+                        Confident: 0,
+                        Fear: 0,
+                        Joy: 0,
+                        Sadness: 0,
+                        Tentative: 0
                         };
 
-var toneArray = ['anger', 'fear', 'joy', 'sadness', 'analytical', 'confident', 'tentative'];
+var toneArray = ['Analytical', 'Anger', 'Confident', 'Fear', 'Joy', 'Sadness', 'Tentative'];
 
 $.ajax(
     { url: watsonUrl, 
@@ -22,15 +22,17 @@ $.ajax(
     success: function(watsonData) {
         var toneObject = watsonData.document_tone.tones 
         for (var i = 0; i < toneObject.length; i++) {
-            var watsonToneId = toneObject[i].tone_id
+            var watsonToneName = toneObject[i].tone_name
             for (var j = 0; j < toneArray.length; j++) {
                 var toneName = toneArray[j]
-                if (watsonToneId === toneName) {
+                if (watsonToneName === toneName) {
                     toneChartObject[toneName] = toneObject[i].score;
                 }
             }  
         }      
     }
     });
+
+    console.log(toneChartObject);
 
 
